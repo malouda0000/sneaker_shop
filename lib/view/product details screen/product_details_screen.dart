5d50633469e41068398e2c446ed87379a6450442
malouda0000 <hamam.hamza85@gmail.com>
2024-11-07 13:14:34 +0400

    final commit

--- lib/view/product details screen/product_details_screen.dart
+++ lib/view/product details screen/product_details_screen.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sneaker_shop/core/theme/colors.dart';
 import 'package:sneaker_shop/models/sneakers%20models/sneakers_models.dart';
+import 'package:sneaker_shop/view/shared/screen/under_develop_screen.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   final Sneaker sneaker;
@@ -12,8 +15,21 @@ class ProductDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(sneaker.name),
-      ),
+          iconTheme: const IconThemeData(color: Colors.white),
+          title: Text(
+            sneaker.name,
+            style: const TextStyle(color: Colors.white),
+          ),
+          backgroundColor: AppColors.mainColor,
+          actions: [
+            IconButton(
+              onPressed: () {
+                Get.to(const UnderDevScreen());
+              },
+              icon: const Icon(Icons.favorite),
+              // color: Colors.white,
+            ),
+          ]),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -29,11 +45,14 @@ class ProductDetailsScreen extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Text(sneaker.name, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              child: Text(sneaker.name,
+                  style: const TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold)),
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Text('\$${sneaker.price}', style: const TextStyle(fontSize: 20, color: Colors.green)),
+              child: Text('\$${sneaker.price}',
+                  style: const TextStyle(fontSize: 20, color: Colors.green)),
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
