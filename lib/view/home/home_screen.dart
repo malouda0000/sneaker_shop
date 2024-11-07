a770d7bd2d636e71c969dfcff95b2f8320d2d0cd
malouda0000 <hamam.hamza85@gmail.com>
2024-11-07 12:25:49 +0400

    basic home screen

---
+++ lib/view/home/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:sneaker_shop/view/home/controller/sneakers_controller.dart';
+
+class HomeScreen extends StatelessWidget {
+  final SneakerController sneakerController = Get.put(SneakerController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sneaker Shop'),
+      ),
+      body: Obx(() {
+        if (sneakerController.sneakers.isEmpty) {
+          return Center(child: CircularProgressIndicator());
+        }
+        return ListView.builder(
+          itemCount: sneakerController.sneakers.length,
+          itemBuilder: (context, index) {
+            final sneaker = sneakerController.sneakers[index];
+            return ListTile(
+              leading: Image.network(sneaker.images[0]),
+              title: Text(sneaker.name),
+              subtitle: Text('\$${sneaker.price}'),
+              onTap: () {
+                // Get.to(() => ProductDetailsScreen(sneaker: sneaker));
+              },
+            );
+          },
+        );
+      }),
+    );
+  }
+}
