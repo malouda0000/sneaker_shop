8eb226d589601e4bdb024fec9eb710dcf2b0f685
malouda0000 <hamam.hamza85@gmail.com>
2024-11-07 13:09:48 +0400

    adding more featucher to the home screen

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:sneaker_shop/core/theme/colors.dart';
 import 'package:sneaker_shop/view/home/controller/sneakers_controller.dart';
-import 'package:sneaker_shop/view/product%20details%20screen/product_details_screen.dart';
+import 'package:sneaker_shop/view/product%20details%20screen/widgets/custom_sneaker_listtile.dart';
+import 'package:sneaker_shop/view/shared/screen/under_develop_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   final SneakerController sneakerController = Get.put(SneakerController());
@@ -11,24 +12,40 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sneaker Shop'),
+          iconTheme: const IconThemeData(color: Colors.white),
+          title: const Text(
+            'Sneaker Shop',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: AppColors.mainColor,
+          actions: [
+            IconButton(
+              onPressed: () {
+                Get.to(const UnderDevScreen());
+              },
+              icon: const Icon(Icons.search),
+              // color: Colors.white,
+            ),
+            IconButton(
+              onPressed: () {
+                Get.to(const UnderDevScreen());
+              },
+              icon: const Icon(Icons.settings),
+              // color: Colors.white,
+            ),
+          ]),
+      drawer: const Drawer(
+        child: Center(child: Text("Drawer")),
       ),
       body: Obx(() {
         if (sneakerController.sneakers.isEmpty) {
-          return Center(child: CircularProgressIndicator());
+          return const Center(child: CircularProgressIndicator());
         }
         return ListView.builder(
           itemCount: sneakerController.sneakers.length,
           itemBuilder: (context, index) {
             final sneaker = sneakerController.sneakers[index];
-            return ListTile(
-              leading: Image.network(sneaker.images[0]),
-              title: Text(sneaker.name),
-              subtitle: Text('\$${sneaker.price}'),
-              onTap: () {
-                Get.to(() => ProductDetailsScreen(sneaker: sneaker));
-              },
-            );
+            return CustomSneakerListTile(sneaker: sneaker);
           },
         );
       }),
